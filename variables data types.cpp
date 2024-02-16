974c3b47350a6990cffafd49bddc5b02489e076e
Anubhav Jaiswal <119493362+Anubhavjaiswal21@users.noreply.github.com>
2024-02-17 00:54:28 +0530

    Add files via upload

---
+++ variables data types.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=9;
+	int b=8;
+	cout<<"THE VALUE OF A IS "<<a<<"\nTHE VALUE OF B IS "<<b;
+	float p=65.9;
+	cout<<"\nTHE VALUE OF p IS "<<p;
+	double m=1.235879467;
+	cout<<"\nTHE VALUE OF m IS "<<m;
+	bool valid=true;
+	cout<<"\nTHE VALUE OF VALID IS "<<valid;
+	
+}
